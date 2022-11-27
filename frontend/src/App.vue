b1056bd2f1b4ecd071399ce531c6a19469a29075
nohehf <nohe.hinniger.foray@gmail.com>
2022-11-27 20:30:29 +0100

    feat: connect front and back

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,23 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from "vue";
+import Page from "./components/Page.vue";
+
+const currentPath = ref(window.location.pathname);
+
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.pathname;
+});
+
+const pageTitle = computed(() => {
+  return currentPath.value.replace("/", "");
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div v-if="currentPath === '/'">HOME</div>
+  <div v-else>
+    <Page :title="pageTitle" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ frontend/src/components/Page.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const props = defineProps<{ title: string }>();
+
+let content = ref("");
+
+async function fetchPage() {
+  fetch("http://localhost:3000/" + props.title)
+    .then((response) => response.text())
+    .then((data) => {
+      content.value = data;
+    });
+}
+
+onMounted(() => {
+  console.log("component mounted");
+  fetchPage();
+});
+</script>
+
+<template>
+  {{ title }}
+  {{ content }}
+</template>
+
+<style scoped></style>
